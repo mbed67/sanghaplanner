7ff31243ed6df14c90c3163264c55359acf7e053
mbed67 <mbed@live.nl>
2015-01-25 11:30:21 +0000

    Improved layout notifications

--- app/views/notifications/index.blade.php
+++ app/views/notifications/index.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>Meldingen voor {{ $user->firstname }} {{ $user->middlename }} {{ $user->lastname }}</h1>
+	<h2>Meldingen voor {{ $user->firstname }} {{ $user->middlename }} {{ $user->lastname }}</h2>
 	@if ($user->notifications->count())
 	<div class="row">
         <div class="col-md-6">
 			@foreach($user->notifications as $notification)
-				<div class="notification {{ $notification->type }}">
-					<p class="timestamp">{{ $notification->present()->timeWhenCreated() }}</p>
-				    <p class="notification-body">{{ $notification->body }}</p>
-
-			        @if($notification->type == "MembershipRequest" && $notification->hasValidObject())
-			            <a href="/sanghas/{{ $notification->getObject()->id }}">Ga naar de sanghapagina</a>
-			        @endif
+				<div class="list-group">
+					@include('notifications.partials.notification')
 				</div>
 			@endforeach
 	@else
--- app/views/notifications/partials/notification.blade.php
+++ app/views/notifications/partials/notification.blade.php
@@ -1,10 +1,11 @@
-<li>
-	<div class="nav-notification">
-		<p class="timestamp">{{ $notification->present()->timeWhenCreated() }}</p>
-		<p class="notification-body">{{ $notification->body }}</p>
-
+<li class="list-group-item">
 	    @if($notification->type == "MembershipRequest" && $notification->hasValidObject())
-	       <a href="/sanghas/{{ $notification->getObject()->id }}">Ga naar de sanghapagina</a>
+	       <a href="/sanghas/{{ $notification->getObject()->id }}">
+				<p class="timestamp">{{ $notification->present()->timeWhenCreated() }}</p>
+				<p class="notification-body">{{ $notification->body }}</p>
+	       </a>
+	    @else
+	    	<p class="timestamp">{{ $notification->present()->timeWhenCreated() }}</p>
+	    	<p class="notification-body">{{ $notification->body }}</p>
 	    @endif
-    </div>
 </li>
