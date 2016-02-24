4c204d92eb2031bab4120069ea43f6c129bc74a1
Marieke Bednarczyk <mbed@live.nl>
2016-02-24 21:26:10 +0100

    setting up react and redux and switch to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    // specifies where the config directory is
+    var options = {
+        config: {
+            src: "grunt/*.json"
+        }
+    };
+
+    //loads the various task configuration files
+    var configs = require('load-grunt-configs')(grunt, options);
+    grunt.initConfig(configs);
+
+    grunt.registerTask(
+        'default',
+        [
+            'sass',
+            'browserify',
+            'uglify'
+        ]
+    );
+
+};
